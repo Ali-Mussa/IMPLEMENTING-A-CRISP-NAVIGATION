199074bc6f4b2b83f704b7293e8db99f3f6c6bcc
Ali-Mussa <alimuse526@gmail.com>
2025-03-04 01:07:17 +0300

    Update StudentMSTest.java
    i updated the test code

--- ModuleTask/src/test/java/org/example/StudentMSTest.java
+++ ModuleTask/src/test/java/org/example/StudentMSTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import javax.swing.*;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 import static org.junit.jupiter.api.Assertions.*;
 
   class StudentMSTest {
